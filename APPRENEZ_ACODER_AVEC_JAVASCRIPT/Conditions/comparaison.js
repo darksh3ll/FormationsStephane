144c66c589856c001ba712f3fd2ba3f1c63c8bdb
darksh3ll <energie.sud83@gmail.com>
2018-01-05 17:32:47 +0100

    creation de l'exercise comparaison.js

---
+++ APPRENEZ_ACODER_AVEC_JAVASCRIPT/Conditions/comparaison.js
@@ -0,0 +1,7 @@
+const num1 = prompt("Entrez un chiffres");
+const num2 = prompt("Entrez un deuxieme chiffres");
+if (num1 < num2) {
+    alert(num1 + " est plus petit que " + num2)
+}else {
+    alert(num2 + " est plus grans que " + num1)
+}

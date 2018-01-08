655a450f1f0bf0157f87bbf01c708499f1948976
darksh3ll <energie.sud83@gmail.com>
2018-01-08 19:45:26 +0100

    creation d`une fonction nombres aleatoires

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 const resulat = [];
 let totalResulat = 0;
 for (let i = 0 ; i < 10 ; i++){
@@ -9,4 +10,14 @@ for (let i = 0 ; i < 10 ; i++){
 
    }
 console.log(resulat);
-console.log("Resultat du tableau = " +totalResulat);
+console.log("Resultat du tableau = " +totalResulat);*/
+
+const sujet = ["stephanie","stephane","stan","ayrton"];
+const verbe = ["mange","saute","tire","suce","attaque"];
+const compl = ["couilles","un mec","une fille","exta terrestre"];
+
+function aleatoire(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+console.log(aleatoire(sujet) + " " + aleatoire(verbe) + " " + " " + aleatoire(compl));
+

df72f446fe5077ed771dcc0a64d7127ff399ea75
darksh3ll <energie.sud83@gmail.com>
2018-01-08 11:33:47 +0100

    creation d'un dossier entrainement boucles

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
+//prend 10 chiffres u hasard grace a la boucle et  place dans un tableau
+// ensuite aditionne la somme du tableau
+
 const resulat = [];
 let totalResulat = 0;
+//Boucles qui prend 10 chiffres au hasard
 for (let i = 0 ; i < 10 ; i++){
     const chiffres = Math.round(Math.random()*100);
     resulat.push(chiffres)

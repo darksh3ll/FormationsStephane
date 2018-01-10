189bad6527e02b8d9694453f62e347bf003c8275
darksh3ll <energie.sud83@gmail.com>
2018-01-10 20:45:08 +0100

    quelques modif

---
+++ APPRENEZ_ACODER_AVEC_JAVASCRIPT/Boucles/saisie.js
@@ -0,0 +1,4 @@
+var nombre = 0; // Valeur initiale permettant d'entrer dans la boucle
+while ((nombre < 50) || (nombre > 100)) {
+    nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));
+}

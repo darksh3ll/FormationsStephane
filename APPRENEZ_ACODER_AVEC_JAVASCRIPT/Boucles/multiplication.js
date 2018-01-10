884a5d330c4021204961578e019a733a6dbae0fa
darksh3ll <energie.sud83@gmail.com>
2018-01-10 22:46:19 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ APPRENEZ_ACODER_AVEC_JAVASCRIPT/Boucles/multiplication.js
@@ -0,0 +1,9 @@
+let num = 0 ; //Valeurs permetant de rentrer dans le boucles
+while ((num < 2) || (num > 9)) {
+    num = Number(prompt("Entrez un nombres entre 2 et 9 "))
+}
+let i = 1 ;
+while (i < 10) {
+    console.log(`${num} x ${i}`);
+i++
+}

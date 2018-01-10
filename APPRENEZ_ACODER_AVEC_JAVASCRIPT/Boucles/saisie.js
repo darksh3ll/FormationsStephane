c2385d49d0900aee4529642e63db88e085f5473c
darksh3ll <energie.sud83@gmail.com>
2018-01-10 20:48:51 +0100

    ajout de todo

--- APPRENEZ_ACODER_AVEC_JAVASCRIPT/Boucles/saisie.js
+++ APPRENEZ_ACODER_AVEC_JAVASCRIPT/Boucles/saisie.js
@@ -1,4 +1,5 @@
+//TODO apprendre la boucle
 var nombre = 0; // Valeur initiale permettant d'entrer dans la boucle
 while ((nombre < 50) || (nombre > 100)) {
-    nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));
+    nombre = Number(prompt("Entrez un nombre entre 50 et 100 : "));//TODO rerentrer dasn une bloucles
 }
